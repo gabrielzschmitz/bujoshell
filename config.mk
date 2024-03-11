0a50c425e014cd1530dff642e7880029ade72cd0
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2024-03-11 09:38:24 -0300

    ⚙ Fixing MacOS build + adding data folder

--- config.mk
+++ config.mk
@@ -1,19 +1,25 @@
-# Compiler and flags
-CC = gcc
-TCCFLAGS = -Wwrite-strings
-GCCFLAGS = -Wextra
-CFLAGS = -std=c99 $(GCCFLAGS) -Wall -pedantic -O3 $(DFLAGS)
-
 # Directories
 PREFIX = /usr/local
 APPPREFIX = $(PREFIX)/share/applications
+DATAPREFIX = .local/share/bujoshell
 INCLUDES = -I$(PREFIX)/include
 LIBDIR = -L$(PREFIX)/lib
-
-# Dependency flags
-DEPFLAGS = -MD
 
 # Combine flags
 CPPFLAGS += $(INCLUDES) $(DEPFLAGS)
 LDFLAGS += $(LIBDIR)
+ifdef __APPLE__
+LDLIBS = -lncurses -lsqlite3
+else
 LDLIBS = $(shell pkg-config --libs ncursesw sqlite3)
+endif
+
+# Dependency flags
+DEPFLAGS = -MD
+
+# Compiler and flags
+CC = gcc
+TCCFLAGS = -Wwrite-strings
+GCCFLAGS = -Wextra
+DFLAGS = -DDATADIR=\"$(DATAPREFIX)\" -DDBFILE=\"$(DATAPREFIX)/bujoshell.db\"
+CFLAGS = -std=c99 $(GCCFLAGS) -Wall -pedantic -O3 $(DFLAGS)
